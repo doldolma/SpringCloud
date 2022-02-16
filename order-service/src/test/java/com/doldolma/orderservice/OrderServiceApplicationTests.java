6a83b8210c626e38b483fe9234adf5a4c79b0bc7
dolma <dolma@gridwiz.com>
2022-02-16 23:50:39 +0900

    order, catalog 서비스구현

---
+++ order-service/src/test/java/com/doldolma/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doldolma.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
